475a81f9644ae035d08d660b08635d07d2c5e61b
mortegaj <marcmordigan@gmail.com>
2023-12-02 09:20:39 +0100

    Create 16x16 squares

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,2 +1,14 @@
-const h1 = document.querySelector("h1");
-h1.textContent = h1.textContent + " with javascript";
+const xSize = 16;
+const ySize = 16;
+
+const container = document.querySelector("#container");
+let square;
+
+for (let i=0; i<xSize; i++) {
+    for (let j=0; j<ySize; j++) {
+        square = document.createElement("div");
+        square.classList.add("square");
+        square.textContent = i+"."+j;
+        container.appendChild(square);
+    }
+}

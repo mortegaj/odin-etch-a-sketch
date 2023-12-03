e4b6c7e889b25a7794641a9e838430e20e2601eb
mortegaj <marcmordigan@gmail.com>
2023-12-03 09:53:37 +0100

    Add change grid size button

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,26 +1,53 @@
-const gridSize = 16;
+let gridSize = 16;
 
 const container = document.querySelector("#container");
 const containerWidth = container.clientWidth;
 const containerHeight = container.clientHeight
-console.log(`Contanineer Size - Width: ${containerWidth} Height: ${containerHeight}`);
 
-const squareWidth = containerWidth / gridSize;
-const squareHeight = containerHeight / gridSize;
-console.log(`Sqare Size - Width: ${squareWidth} Height: ${squareHeight}`);
+const btn = document.querySelector("#change-size-btn");
+btn.addEventListener("click", changeGridSize);
 
-for (let i=0; i<gridSize; i++) {
-    for (let j=0; j<gridSize; j++) {
-        square = document.createElement("div");
-        square.classList.add("square");
-        square.style.cssText = "width: " + squareWidth + "px; height: " + squareHeight + "px;"; 
-        
-        container.appendChild(square);
+const span = document.querySelector("#number-squares");
 
-        square.addEventListener ("mouseover", paintSquare);
+drawCanvas();
+
+function drawCanvas() {
+    const squareWidth = containerWidth / gridSize;
+    const squareHeight = containerHeight / gridSize;
+    let square;
+    
+    for (let i=0; i<gridSize; i++) {
+        for (let j=0; j<gridSize; j++) {
+            square = document.createElement("div");
+            square.classList.add("square");
+            square.style.cssText = "width: " + squareWidth + "px; height: " + squareHeight + "px;"; 
+            
+            container.appendChild(square);
+
+            square.addEventListener("mousedown", paintSquare);
+            square.addEventListener("mouseover", paintSquare);
+        }
     }
 }
 
 function paintSquare(e) {
-    if (e.buttons === 1) e.target.style.backgroundColor = "red";
+    if (e.buttons) e.target.style.backgroundColor = "red";
+}
+
+function changeGridSize () {
+    const newGridSize = parseInt(prompt("Choose grid size: "));
+    if (newGridSize <= 100) {
+        gridSize = newGridSize;
+        span.textContent = gridSize;
+
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        }
+
+        drawCanvas();
+
+    } else {
+        alert ("max value = 100");
+    }
+    
 }

f3933d09c2f3948bf48cbfb97b7fc7f4de9607f6
Marc Ortega <marcmordigan@gmail.com>
2025-03-01 19:58:49 +0100

    Create a webpage with a 16x16 grid of square divs

--- javascript.js
+++ javascript.js
@@ -1,1 +1,20 @@
-console.log("Hello World!");
+const container = document.querySelector("#container");
+
+function createGrid (height, width) {
+    //Create "height" DIVs, one per line
+    for (let i = 0; i < height; i++ ) {
+        const line = document.createElement("div");
+        line.classList.add("line");
+        
+        //Create "width" DIVs within the line, one per square
+        for (let j = 0; j < width; j++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            line.appendChild(square);
+        }
+        
+        container.appendChild(line);
+    }
+}
+
+createGrid (16,16);
